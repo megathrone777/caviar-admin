f87f891625e698ab24e8029c8a1412abf097b2f0
megathrone777 <krol2006@bigmir.net>
2021-10-30 03:59:05 +0200

    Some

--- api/product/models/product.js
+++ api/product/models/product.js
@@ -21,5 +21,12 @@ module.exports = {
         data.slug = slugify(data.title, { lower: true });
       }
     },
+    async beforeSave(model, attrs, options) {
+      if (options.method === "insert" && attrs.title) {
+        model.set("slug", slugify(attrs.title, { lower: true }));
+      } else if (options.method === "update" && attrs.title) {
+        attrs.slug = slugify(attrs.title, { lower: true });
+      }
+    },
   },
 };
